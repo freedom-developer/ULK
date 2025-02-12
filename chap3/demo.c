261abc0510dcc38f0762f2c4805e07a7c7abbfc7
fadf <abc@123.com>
2025-02-11 23:37:00 -0800

    update

--- chap3/demo.c
+++ chap3/demo.c
@@ -4,9 +4,13 @@
 #include <linux/syscalls.h> // SYSCALL_DEFINEx
 #include <linux/moduleparam.h>
 #include <linux/wait.h>
+#include <linux/kthread.h>
+#include <linux/delay.h>
+#include <linux/kernel.h>
+#include <asm/unistd.h>
 
 #ifndef NR_syscalls
-#define NR_syscalls 448
+#define NR_syscalls 333
 #endif
 
 int demo_wait_nr = -1;
@@ -17,6 +21,9 @@ module_param(demo_wakeup_nr, int, 0444); // 只读
 
 DECLARE_WAIT_QUEUE_HEAD(demo_wait_queue);
 int demo_wait_condition = 0;
+
+int sc_stat[NR_syscalls];
+unsigned long old_scs[NR_syscalls];
 
 // 系统调用的实现
 asmlinkage long demo_wait(void)
@@ -40,6 +47,20 @@ asmlinkage long demo_wakeup(void)
 }
 
 
+
+// 内核线程的实现
+int thread_fn(void *data)
+{
+    printk("demo_thread started\n");
+    while (!kthread_should_stop()) {
+        printk("demo_thread running\n");
+        msleep(1000);
+    }
+    printk("demo_thread stopped\n");
+    return 0;
+}
+
+struct task_struct *kthread_demo = NULL;
 
 // 创建一个系统调用
 int create_syscall(unsigned long syscall_addr)
@@ -120,17 +141,25 @@ static int __init demo_init(void)
     printk("demo init\n");   
 
     // 新建系统调用
-    if ((ret = create_syscall((unsigned long)demo_wait)) < 0) {
-        printk("create_syscall demo_wait failed\n");
+    // if ((ret = create_syscall((unsigned long)demo_wait)) < 0) {
+    //     printk("create_syscall demo_wait failed\n");
+    //     return -1;
+    // }
+    // demo_wait_nr = ret;
+    // if ((ret = create_syscall((unsigned long)demo_wakeup)) < 0) {  
+    //     printk("create_syscall demo_wakeup failed\n");
+    //     return -1;
+    // }
+    // demo_wakeup_nr = ret;
+    // printk("demo_wait_nr = %d, demo_wakeup_nr = %d\n", demo_wait_nr, demo_wakeup_nr);
+
+    // 创建一个内核线程
+    kthread_demo = kthread_run(thread_fn, NULL, "kthread_demo");
+    if (IS_ERR(kthread_demo)) {
+        printk("kthread_run failed\n");
         return -1;
     }
-    demo_wait_nr = ret;
-    if ((ret = create_syscall((unsigned long)demo_wakeup)) < 0) {  
-        printk("create_syscall demo_wakeup failed\n");
-        return -1;
-    }
-    demo_wakeup_nr = ret;
-    printk("demo_wait_nr = %d, demo_wakeup_nr = %d\n", demo_wait_nr, demo_wakeup_nr);
+    printk("kthread_demo = %p\n", kthread_demo);
 
     return 0;
 }
@@ -141,6 +170,7 @@ static void __exit demo_exit(void)
 {
     printk("demo_exit\n");
 
+    // 移除系统调用
     if (demo_wait_nr > 0) {
         remove_syscall(demo_wait_nr);
     }
@@ -148,6 +178,11 @@ static void __exit demo_exit(void)
         remove_syscall(demo_wakeup_nr);
     }
 
+    // 移除内核线程
+    if (kthread_demo) {
+        kthread_stop(kthread_demo);
+        kthread_demo = NULL;
+    }
 
 }
 
